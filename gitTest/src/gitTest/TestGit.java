037ccaa7075c6d9b42f6a67d427c43069eadc8a8
Essomba Bonaventure <jessomba@gmail.com>
2019-03-22 01:00:58 +0100

    Hello mine

--- gitTest/src/gitTest/TestGit.java
+++ gitTest/src/gitTest/TestGit.java
@@ -5,6 +5,8 @@ public class TestGit {
 	public static void main(String[] args) {
 		System.out.println("Hello Java and Git");
 		System.out.println();
+		System.out.println("Hello from branch bug#1234!!");
+		System.out.println("Oh yesssss");
 	}
 
 }
